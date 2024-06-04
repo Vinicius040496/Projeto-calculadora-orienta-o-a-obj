a078850e367d7c46ec909fbd0d11e967f6a48bf7
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-04 14:38:25 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp128/Calculadora.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp128
+{
+    internal class Calculadora
+    {
+        public double ra;
+        public double Circunferencia;
+        public double Volume;
+        public double Pi = 3.14;
+
+        public double circunf()
+        {
+            return 2* Pi * ra;
+        }
+
+        public double Vol()
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(ra, 3);
+        }
+
+        public double P()
+        {
+            return 3.14;
+        }
+    }
+}
---
+++ ConsoleApp128/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp128
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora Rai = new Calculadora();
+
+            Console.WriteLine("Qual valor do raio: ");
+            Rai.ra = double.Parse(Console.ReadLine());
+            Rai.circunf();
+            Console.WriteLine("Circunfêrencia do raio é {0:f2}",Rai.circunf());
+            Rai.Vol();
+            Console.WriteLine("Volume do raio é {0:f2}", Rai.Vol());
+            Rai.P();
+            Console.WriteLine("Valor de Pi é {0:f2}",Rai.P());
+            Console.ReadLine();
+        }
+    }
+}
